82f27163fcef9c32ade7061a63e7eec06208a38e
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-05-21 22:36:41 -0700

    added workflows file

---
+++ build/precache-manifest.79cfc6e3bfd9c2cfa8025b466dc7a4f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fefee9dce326dbef475ee1dd2ecd34ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d94a683a0eefbb8329dc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "23517e84aa2f220351de",
+    "url": "/static/js/2.6aa44168.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6aa44168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d94a683a0eefbb8329dc",
+    "url": "/static/js/main.c9b76a27.chunk.js"
+  },
+  {
+    "revision": "66a1f48ff5ce6cb141e1",
+    "url": "/static/js/runtime-main.3f9a3c9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
